0abbfef9f86684e74d3d84454409c652e5dbafbb
emanuel <emanuel@localhost.localdomain>
2020-06-07 23:02:03 -0300

    tp10

---
+++ tp10/build-txtBuscador-Desktop-Debug/ui_mainwindow.h
@@ -0,0 +1,106 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.13.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCommandLinkButton>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLineEdit *lineEdit;
+    QCommandLinkButton *commandLinkButton;
+    QTableView *tableView;
+    QTextEdit *textEdit;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(718, 433);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+        commandLinkButton = new QCommandLinkButton(centralwidget);
+        commandLinkButton->setObjectName(QString::fromUtf8("commandLinkButton"));
+
+        horizontalLayout->addWidget(commandLinkButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+        tableView->setEnabled(true);
+
+        verticalLayout->addWidget(tableView);
+
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setEnabled(true);
+        textEdit->setReadOnly(true);
+
+        verticalLayout->addWidget(textEdit);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 718, 29));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        commandLinkButton->setText(QCoreApplication::translate("MainWindow", "Buscar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ tp10/txtBuscador/mainwindow.cpp
@@ -0,0 +1,58 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDir>
+#include <QStandardItemModel>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->lineEdit->setText("/");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_commandLinkButton_clicked()
+{
+    QDir directory(this->ui->lineEdit->text());
+    QStringList files = directory.entryList(QStringList() << "*.txt",QDir::Files);
+    QStandardItemModel * model = new QStandardItemModel();
+    int i = 0;
+    model->setColumnCount(2);
+    model->setHorizontalHeaderItem(0, new QStandardItem("archivos"));
+    model->setHorizontalHeaderItem(1, new QStandardItem("cantidad"));
+    foreach(QString filename, files) {
+        QStandardItem * item = new QStandardItem(filename);
+        model->setItem(i, 0, item);
+        model->setItem(i, 1, new QStandardItem(QString::number(0)));
+        i++;
+    }
+    this->ui->tableView->setModel(model);
+}
+
+void MainWindow::on_tableView_doubleClicked(const QModelIndex &index)
+{
+    QVariant item = this->ui->tableView->model()->data(index);
+    QString filename = this->ui->lineEdit->text() + "/" + item.toString();
+    std::ifstream archivo;
+    archivo.open (filename.toStdString().c_str());
+    std::string line;
+    std::string totalline="";
+    if (archivo.is_open())
+    {
+        while ( getline (archivo,line) )
+        {
+              totalline = totalline + line + '\n';
+        }
+        this->ui->textEdit->setText(QString(totalline.c_str()));
+    }
+    archivo.close();
+}
